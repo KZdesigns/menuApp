bdf8632cb348badceb22ef16c4899182f90bc77d
Kyle Zimmerman <kzdesignsios@gmail.com>
2022-07-24 14:13:29 -0700

    project complete added bump effect to cart, finished reducer logic for adding item and removing items from the cart

--- src/components/Layout/HeaderCardButton.js
+++ src/components/Layout/HeaderCardButton.js
@@ -1,17 +1,37 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import CartIcon from "../Cart/CartIcon";
 import classes from "../Layout/HeaderCardButton.module.css";
 import CartContext from "../../store/cart-context";
 
 const HeaderCardButton = (props) => {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
   const Cartctx = useContext(CartContext);
+  const { items } = Cartctx;
 
-  const numberOfCartItems = Cartctx.items.reduce((currNumber, item) => {
+  const numberOfCartItems = items.reduce((currNumber, item) => {
     return currNumber + item.amount;
   }, 0);
 
+  const btnClasses = `${classes.button} ${
+    btnIsHighlighted ? classes.bump : ""
+  }`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnIsHighlighted(true);
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
