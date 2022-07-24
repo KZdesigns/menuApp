81a2d6dc1eaa37a4e6cd44925d964c2a9c02db95
Kyle Zimmerman <kzdesignsios@gmail.com>
2022-07-23 22:29:23 -0700

    Added context for the cart and provider, user can now add items to the cart and the cart items updates in the UI

--- src/components/Layout/HeaderCardButton.js
+++ src/components/Layout/HeaderCardButton.js
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
 import CartIcon from "../Cart/CartIcon";
 import classes from "../Layout/HeaderCardButton.module.css";
+import CartContext from "../../store/cart-context";
 
 const HeaderCardButton = (props) => {
+  const Cartctx = useContext(CartContext);
+
+  const numberOfCartItems = Cartctx.items.reduce((currNumber, item) => {
+    return currNumber + item.amount;
+  }, 0);
+
   return (
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
